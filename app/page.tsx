9edf647ad2a9e271f9e753a7e855716bf6dd1fef
Madhuranga Senadheera <lilan.maduranga@gmail.com>
2025-04-11 00:19:18 +0400

    Refactor navigation links to use a reusable component for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Link from 'next/link';
 
+const lnk = (url: string, label: string) => (
+  <Link href={url} target="_blank" rel="noopener noreferrer">
+    {label}
+  </Link>
+);
+
 const Page = () => {
   return (
     <div className="p-6">
@@ -10,17 +16,15 @@ const Page = () => {
             <Link href="/stock-news">Stock News Youtubes</Link>
           </li>
           <li>
-            <Link href="https://finviz.com/map.ashx" target="_blank">Stock Maps</Link> |&nbsp;
-            <Link
-              href="http://tradingview.com/heatmap/stock/#%7B%22dataSource%22%3A%22SPX500%22%2C%22blockColor%22%3A%22change%22%2C%22blockSize%22%3A%22market_cap_basic%22%2C%22grouping%22%3A%22sector%22%7D"
-              target="_blank"
-            >
-              Stock Map 2 - TradingView
-            </Link> |&nbsp;
-            <Link href="https://coinmarketcap.com/crypto-heatmap/" target="_blank">Crypto Maps</Link>
+            {lnk("https://finviz.com/map.ashx", "Stock Maps")} |&nbsp;
+            {lnk(
+              "http://tradingview.com/heatmap/stock/#%7B%22dataSource%22%3A%22SPX500%22%2C%22blockColor%22%3A%22change%22%2C%22blockSize%22%3A%22market_cap_basic%22%2C%22grouping%22%3A%22sector%22%7D",
+              "Stock Map 2 - TradingView"
+            )} |&nbsp;
+            {lnk("https://coinmarketcap.com/crypto-heatmap/", "Crypto Maps")}
           </li>
           <li>
-            <Link href="https://www.khaleejtimes.com/gold-forex?botrequest=true">Rates | Gold - Currency </Link>
+            {lnk("https://www.khaleejtimes.com/gold-forex?botrequest=true", "Rates | Gold - Currency")}
           </li>
         </ul>
       </nav>
