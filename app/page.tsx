ceacf7b7b68d3ab6300c0a4bb0ea338bc5b86afc
Madhuranga Senadheera <lilan.maduranga@gmail.com>
2025-04-11 00:10:03 +0400

    Enhance navigation links and improve styling on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,26 @@ import Link from 'next/link';
 
 const Page = () => {
   return (
-    <div>
-      <h1>My Views</h1>
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">My Views</h1>
       <nav>
-        <ul>
+        <ul className="list-disc pl-5 space-y-2">
           <li>
-            <Link href="/stock-news">Stock-news</Link>
+            <Link href="/stock-news">Stock News Youtubes</Link>
           </li>
-          {/* Add more links as needed */}
+          <li>
+            <Link href="https://finviz.com/map.ashx" target="_blank">Stock Maps</Link> |&nbsp;
+            <Link
+              href="http://tradingview.com/heatmap/stock/#%7B%22dataSource%22%3A%22SPX500%22%2C%22blockColor%22%3A%22change%22%2C%22blockSize%22%3A%22market_cap_basic%22%2C%22grouping%22%3A%22sector%22%7D"
+              target="_blank"
+            >
+              Stock Map 2 - TradingView
+            </Link> |&nbsp;
+            <Link href="https://coinmarketcap.com/crypto-heatmap/" target="_blank">Crypto Maps</Link>
+          </li>
+          <li>
+            <Link href="https://www.khaleejtimes.com/gold-forex?botrequest=true">Rates | Gold - Currency </Link>
+          </li>
         </ul>
       </nav>
     </div>
